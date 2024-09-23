aff10132d7cc2af48c845249034c77375685f812
Kevin Barrios <barrik3@unlv.nevada.edu>
2024-09-23 12:13:36 -0700

    Files From Previous Lab

---
+++ src/counter.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from . import status
+
+app = Flask(__name__)
+
+COUNTERS = {}
+
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    ''' Create a counter '''
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        return {"Messgae": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+
+
+
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    ''' Update a counter '''
+    app.logger.info(f"Request to update counter: {name}")
+    global COUNTERS
+    if name not in COUNTERS:
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+    COUNTERS[name] += 1
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+@app.route('/counters/<name>', methods=['GET'])
+def read_counter(name):
+    ''' Read from counter '''
+    app.logger.info(f"Request to read from counter: {name}")
+    global COUNTERS
+    if name not in COUNTERS:
+        return {"Message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+
+
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,76 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+import pytest
+from src.counter import app
+from src import status
+
+@pytest.fixture()
+def client():
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndPoints:
+    '''Test cases for counter related endpoints'''
+
+    def test_create_a_counter(self, client):
+        ''' It should create a counter '''
+        result = client.post('/counters/foo')
+        assert result.status_code == status.HTTP_201_CREATED
+
+    def test_duplicate_a_counter(self, client):
+        ''' It should return error for duplicates '''
+        result = client.post('/counters/bar')
+        assert result.status_code == status.HTTP_201_CREATED
+        result = client.post('/counters/bar')
+        assert result.status_code == status.HTTP_409_CONFLICT
+
+    def test_update_a_counter(self, client):
+        # create a counter
+        result = client.post('/counters/my_counter')
+        assert result.status_code == status.HTTP_201_CREATED
+
+        # save the base of our counter and make sure we start at 0
+        data = result.json
+        baseline = data['my_counter']
+        assert baseline == 0
+
+        # call the update counter method
+        result2 = client.put(f"/counters/my_counter")
+
+        #make sure that our update was successful.
+        assert result2.status_code == status.HTTP_200_OK
+
+        #make sure that are counters data is now one more than previous
+        # baseline. 
+        data2 = result2.json
+        curr_value = data2['my_counter']
+        assert curr_value == baseline + 1
+
+
+    def test_read_a_counter(self, client):
+        #create a counter
+        result = client.post('/counters/some_counter')
+        assert result.status_code == status.HTTP_201_CREATED
+
+        #hold counter
+        counter = result.json['some_counter']
+
+        #read from counter
+        counter_name = "some_counter"
+        result2 = client.get(f'/counters/{counter_name}')
+        assert result2.status_code == status.HTTP_200_OK
+
+        #compare the counter we are reading with the original counter
+        read_counter = result2.json[counter_name]
+        assert counter == read_counter 
+
+
+    def test_fail_update_a_counter(self, client):
+        result = client.put('/counters/idontexist')
+        assert result.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_fail_read_a_counter(self, client): 
+        result = client.get('/counters/idontexist')
+        assert result.status_code == status.HTTP_404_NOT_FOUND
+
